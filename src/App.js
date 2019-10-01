b5148c099bb31a001ec975f619cb3940d791f553
Evgeny Kiselev <ekiscode@gmail.com>
2019-10-01 15:32:37 +0500

    testd45345

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="app-wrapper">
       <header className="header">
+        fdfdf
         <img
           src="https://bcassetcdn.com/asset/logo/4c8f8b36-51b9-4ebf-b20c-00e5c25719c3/logo?v=4&text=Logo+Text+Here"
           alt=""
